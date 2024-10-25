bc3e187a3f00fef76c3a325c738605b9eeb95043
Fatalan <lilith@yandex.ru>
2024-10-25 15:03:53 +0300

    More cubes

--- CubesRotation/main.cpp
+++ CubesRotation/main.cpp
@@ -77,12 +77,13 @@ void MoveCube(cube& cub) {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(0, 0, 0, 0.5, 0.5, 0.5, 0, 1.0, 0);
+    gluLookAt(0, 0, 0, 0.5, 0.5, 0.5, 0.0, 1.0, 0.0);
+    //gluPerspective(1.14, float(GLUT_WINDOW_WIDTH / GLUT_WINDOW_HEIGHT), float(GLUT_WINDOW_HEIGHT / 2) / tan(1.14 / 2.0) / 10, float(GLUT_WINDOW_HEIGHT / 2) / tan(1.14 / 2.0) * 10);
     glEnable(GL_DEPTH_TEST);
-    //glDepthFunc(GL_LESS);
+    glDepthFunc(GL_LESS);
     for (unsigned int i = 0; i < cubes->size(); i++) {
-        DrawCube(cubes[i][0]);
-        MoveCube(cubes[i][0]);
+        DrawCube((*cubes)[i]);
+        MoveCube((*cubes)[i]);
     }
     glFlush();
 }
@@ -90,7 +91,7 @@ void display() {
 void Loop(int i)
 {
     glutPostRedisplay();
-    glutTimerFunc(1000 / 60, Loop, 0);
+    glutTimerFunc(5000 / 60, Loop, 0);
 }
 
 int main(int argc, char** argv) {
@@ -104,6 +105,26 @@ int main(int argc, char** argv) {
     cub.t3 = glm::vec3(1, 1, 0);
     cub.t4 = glm::vec3(1, 1, 1);
     cubes->insert(cubes->begin(), cub);
+    cube cub2 = *new cube();
+    cub2.b1 = glm::vec3(1, 0, 1);
+    cub2.b2 = glm::vec3(1, 0, 2);
+    cub2.b3 = glm::vec3(2, 0, 1);
+    cub2.b4 = glm::vec3(2, 0, 2);
+    cub2.t1 = glm::vec3(1, 1, 1);
+    cub2.t2 = glm::vec3(1, 1, 2);
+    cub2.t3 = glm::vec3(2, 1, 1);
+    cub2.t4 = glm::vec3(2, 1, 2);
+    cubes->insert(cubes->begin(), cub2);
+    cube cub3 = *new cube();
+    cub3.b1 = glm::vec3(-1, 0, -1);
+    cub3.b2 = glm::vec3(-1, 0, 0);
+    cub3.b3 = glm::vec3(0, 0, -1);
+    cub3.b4 = glm::vec3(0, 0, 0);
+    cub3.t1 = glm::vec3(-1, 1, -1);
+    cub3.t2 = glm::vec3(-1, 1, 0);
+    cub3.t3 = glm::vec3(0, 1, -1);
+    cub3.t4 = glm::vec3(0, 1, 0);
+    cubes->insert(cubes->begin(), cub3);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(0, 0);
